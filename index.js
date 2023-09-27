2b0296ad4944d2370eaf0bfcc5548b06bcb1943f
Pedro Henrique <henriquepedro1912@gmail.com>
2023-09-27 17:10:15 -0300

    Alteração da logica do sistema + depuração, sistema ainda com problema no envio

--- index.js
+++ index.js
@@ -1,89 +1,116 @@
-const express = require('express');
-const { Client } = require('whatsapp-web.js');
+const { Client, MessageMedia } = require('whatsapp-web.js');
 const qrcode = require('qrcode');
 const crypto = require('crypto');
+const express = require('express');
+const http = require('http');
+const fs = require('fs');
+const dotenv = require('dotenv');
 
-const app = express();
-const port = 3000;
+dotenv.config();
+let qrcodeRouteActive = true;
 
-let qrCodeActive = true; // Variável de controle para ativar/desativar o QR code
+const SESSION_FILE_PATH = './session.json';
+const TOKEN_FILE_PATH = '.env';
 
-// Gera uma chave secreta aleatória com 32 bytes (256 bits)
-const secretKey = crypto.randomBytes(32).toString('hex');
-
-const client = new Client();
-
-// Middleware para autenticação por token
-function authenticate(req, res, next) {
-  const token = req.headers['authorization'];
-
-  if (!token || token !== `Bearer ${secretKey}`) {
-    return res.status(401).json({ message: 'Acesso não autorizado' });
-  }
-
-  next();
+// Verifica se o arquivo .env existe e contém um token. Caso contrário, cria um novo token.
+if (!fs.existsSync(TOKEN_FILE_PATH) || !process.env.API_TOKEN) {
+  const token = crypto.randomBytes(32).toString('hex');
+  fs.writeFileSync(TOKEN_FILE_PATH, `API_TOKEN=${token}`);
+  console.log(`Token gerado e salvo no arquivo ${TOKEN_FILE_PATH}`);
 }
 
+const apiToken = process.env.API_TOKEN;
+const app = express();
+let server;
+const client = new Client();
+server = http.createServer(app);
+
+// Middleware para analisar o corpo JSON das solicitações
 app.use(express.json());
 
-client.on('qr', (qrCode) => {
-    // QR code gerado, ativa a rota
-    qrCodeActive = true;
-    console.log('Escaneie o código QR com o WhatsApp para autenticar.');
+client.on('qr', async (qrCode) => {
+  // Exibe o QR code no terminal
+  qrcode.toString(qrCode, { type: 'terminal' }, (err, url) => {
+    if (err) {
+      console.error('Erro ao gerar QR code:', err);
+    } else {
+      console.log(url);
+    }
   });
 
-// Rota para exibir o QR code
-// Rota para exibir o QR code
-app.get('/qrcode', (req, res) => {
+  app.get('/qrcode', async (req, res) => {
     try {
-      if (qrCodeActive) {
-        const qrCode = client.generateQR(); // Gere o QR code usando o cliente do WhatsApp Web
-        qrcode.toDataURL(qrCode, (err, qrDataURL) => {
-          if (err) {
-            console.error('Erro ao criar QR code:', err);
-            res.status(500).send('Erro ao criar QR code.');
-          } else {
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.write(`<img src="${qrDataURL}" alt="QR Code">`);
-            res.end();
-          }
-        });
-      } else {
-        res.status(403).json({ message: 'QR code desativado' });
-      }
+      const qrDataURL = await qrcode.toDataURL(qrCode);
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.write(`<img src="${qrDataURL}" alt="QR Code">`);
+      res.end();
     } catch (err) {
       console.error('Erro ao criar QR code:', err);
       res.status(500).send('Erro ao criar QR code.');
     }
   });
-  
-
-
-
-client.on('authenticated', (session) => {
-  // Sessão autenticada, desativa a rota
-  qrCodeActive = false;
-  console.log('WhatsApp Web autenticado com sucesso!');
 });
 
-// Rota protegida que envia uma mensagem
-app.post('/enviar-mensagem', authenticate, async (req, res) => {
+app.post('/api/enviar-mensagem', (req, res, next) => {
+  console.log('Corpo da Solicitação:', req.body);
+  next(); // Continue com o processamento normal da rota
+}, async (req, res) => {
+  console.log('Authorization Header no Início da Rota:', req.header('Authorization'));
+  const { number, body, medias } = req.body;
+
+  // Verifica se o token de autorização é válido
+  const authorizationHeader = req.header('Authorization');
+  console.log('Authorization Header:', authorizationHeader); //linha para depuração
+  if (!authorizationHeader || authorizationHeader !== `Bearer ${apiToken}`) {
+    return res.status(401).json({ message: 'Token de autorização inválido' });
+  }
+
+  // Verifica se o WhatsApp está autenticado
+  if (!client.isReady) {
+    return res.status(400).json({ message: 'WhatsApp não está autenticado' });
+  }
+
   try {
-    const { numero, mensagem } = req.body;
-    const chat = await client.getChatById(numero);
-    chat.sendMessage(mensagem);
-    res.json({ message: 'Mensagem enviada com sucesso' });
+    let mediaArray = [];
+    if (medias && Array.isArray(medias)) {
+      // Processa as mídias anexadas
+      for (const media of medias) {
+        const mediaBuffer = Buffer.from(media, 'base64');
+        const extension = media.startsWith('data:image/') ? media.split(';')[0].split('/')[1] : 'jpg';
+        const mediaType = media.startsWith('data:video/') ? 'video' : 'image';
+        const mediaName = `${crypto.randomBytes(16).toString('hex')}.${extension}`;
+        fs.writeFileSync(mediaName, mediaBuffer);
+        const mediaObject = new MessageMedia(mediaType, mediaName);
+        mediaArray.push(mediaObject);
+      }
+    }
+
+    // Envia a mensagem
+    const chat = await client.getChatById(number);
+    await chat.sendMessage(body, { media: mediaArray });
+    return res.status(200).json({ message: 'Mensagem enviada com sucesso' });
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Erro ao enviar mensagem' });
+    console.error('Erro ao enviar mensagem:', error);
+    return res.status(500).json({ message: 'Erro ao enviar mensagem' });
   }
 });
 
-app.listen(port, () => {
-  console.log(`API WhatsApp está rodando em http://localhost:${port}`);
+server.listen(3000, () => {
+  console.log('Servidor rodando em http://localhost:3000/qrcode');
 });
 
-// Conecta-se ao WhatsApp Web
-client.initialize();
+client.on('authenticated', (session) => {
+  console.log('Sessão autenticada com sucesso!');
+});
 
-console.log('Chave Aleatória:', secretKey);
+client.on('ready', () => {
+  console.log('Sessão pronta para uso!');
+  // Agora você pode começar a interagir com o WhatsApp aqui
+});
+
+client.on('auth_failure', (msg) => {
+  console.error('Falha na autenticação:', msg);
+});
+
+// Inicie a conexão com o WhatsApp
+client.initialize();
